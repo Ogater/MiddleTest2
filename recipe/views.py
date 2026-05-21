b26259ffa37d5cdaa00bdab97bf66b23bd9f736a
Ogater <iabrl8034@gmail.com>
2026-05-21 16:48:53 +0300

    Add main view returning 10 random recipes

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,9 @@
+from django.shortcuts import render
 
+from .models import Recipe
+
+
+def main(request):
+    """Display up to 10 random recipes, reshuffled on every page load."""
+    recipes = Recipe.objects.order_by('?')[:10]
+    return render(request, 'main.html', {'recipes': recipes})
